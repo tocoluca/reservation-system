6e02b544b46b5c64e7a57b8bba68239852b0e04c
tocoluca <yasai.saien@gmail.com>
2026-03-23 23:37:29 +0900

    260322

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
 use SocialiteProviders\Manager\SocialiteWasCalled;
 use SocialiteProviders\Line\LineExtendSocialite;
+use App\Console\Commands\DemoResetCommand;
+use App\Console\Commands\DemoExportSeedCommand;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
         Event::listen(function (SocialiteWasCalled $event) {
             $event->extendSocialite('line', \SocialiteProviders\Line\Provider::class);
         });
+        $this->commands([
+            DemoResetCommand::class,
+            DemoExportSeedCommand::class,
+        ]);
     }
 }

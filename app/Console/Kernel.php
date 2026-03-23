e137362def712305e97044a34b49602f723de423
tocoluca <yasai.saien@gmail.com>
2026-03-23 23:51:14 +0900

    260322

---
+++ app/Console/Kernel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\DemoResetCommand;
+use App\Console\Commands\DemoExportSeedCommand;
+
+class Kernel extends ConsoleKernel
+{
+    protected $commands = [
+        DemoResetCommand::class,
+        DemoExportSeedCommand::class,
+    ];
+
+    protected function schedule(Schedule $schedule): void
+    {
+        $schedule->command('demo:reset')
+            ->dailyAt('03:00')
+            ->withoutOverlapping();
+    }
+
+    protected function commands(): void
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -8,10 +8,3 @@ use App\Console\Commands\DemoResetCommand;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Artisan::starting(function ($artisan) {
-    $artisan->resolveCommands([
-        DemoExportSeedCommand::class,
-        DemoResetCommand::class,
-    ]);
-});

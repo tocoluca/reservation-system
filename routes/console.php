bdc6029cda535dbe967ede25f086717f92f610c4
tocoluca <yasai.saien@gmail.com>
2026-04-03 09:03:26 +0900

    260330

--- routes/console.php
+++ routes/console.php
@@ -11,11 +11,11 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('mail:send-revisit-reminders')
-    ->dailyAt('09:00')
+    ->dailyAt('12:10')
     ->withoutOverlapping();
 
 Schedule::command('mail:send-reservation-reminders')
-    ->dailyAt('09:00')
+    ->dailyAt('12:10')
     ->withoutOverlapping();
 
 Schedule::command('demo:reset')
